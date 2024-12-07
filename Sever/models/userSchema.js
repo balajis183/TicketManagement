5157198b6eb014762b4951751c7278945a4cfc95
balajis183 <sbalaji2000s@gmail.com>
2024-12-07 08:54:55 +0530

    first commit

---
+++ Sever/models/userSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+//create a schmea for user
+
+const userSchema = mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  contact: Number,
+});
+
+// create a model for user
+
+const User = mongoose.model("User", userSchema);
+
+//export the model
+
+module.exports = User;
+
